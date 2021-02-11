d255de3dccf215bd5fca2c9059f4ea19c497369f
abiodun-laoye <49166421+abiodun-laoye@users.noreply.github.com>
2021-02-11 13:07:38 -0500

    update version number for PyPI distribution archive

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if os.path.exists(requires_file):
 setup(
 
     name='nrc_ngs_dl',
-    version='v1.9.12',
+    version='v1.9.12.2',
     description='software for downloading and handling sequence data from NRC-LIMS website',
     long_description=readme,
     long_description_content_type='text/markdown',
