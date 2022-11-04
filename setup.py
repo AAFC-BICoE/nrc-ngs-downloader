75ec9e88c3df55b2925801bf98f9f083d69950bf
Melanie Belisle-Leclerc <melanie.belisle-leclerc@agr.gc.ca>
2022-11-04 10:52:09 -0400

    Update environment files

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os.path
 
 readme_file = 'README.md'
-readme = 'Cannot find the file:'+readme_file
+readme = 'Cannot find the file:' + readme_file
 if os.path.exists(readme_file):
-
     with open(readme_file) as f:
-
         readme = f.read()
 
 setup(
-
     name='nrc_ngs_dl',
-    version='v2.0,
+    version='v2.0',
     description='software for downloading and handling sequence data from NRC-LIMS website',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -22,15 +19,8 @@ setup(
     packages=find_packages(exclude='test'),
     scripts=['config.ini.sample'],
     entry_points={
-
         'console_scripts': [
-
             'lims_downloader = nrc_ngs_dl.lims_downloader:main',
-
-            ],
-
+        ],
     }
-
 )
-
-
